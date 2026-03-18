608e3d6f3cdfe63d15ceac62267886539024b0e7
Atul Rajput <eatulrajput@gmail.com>
2026-03-18 18:52:00 +0530

    Refactor: Update Pages

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 Installation modules:
 
 - lenis
-- Color : #5ee9b5
+- Color : #5ee9b5 
+- [Color Information](https://coolors.co/5ee9b5)
 
 ```
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -54,7 +54,7 @@ export const Home = () => {
 
         <div className="mx-auto grid w-full max-w-7xl grid-cols-1 gap-4 lg:grid-cols-3">
           <WobbleCard
-            containerClassName="col-span-1 lg:col-span-2 h-full bg-pink-800 min-h-[500px] lg:min-h-[300px]"
+            containerClassName="col-span-1 lg:col-span-2 h-full bg-emerald-700 min-h-[500px] lg:min-h-[300px]"
             className=""
           >
             <div className="max-w-xs">
@@ -73,15 +73,15 @@ export const Home = () => {
               className="absolute -right-4 -bottom-10 rounded-2xl object-contain grayscale filter lg:-right-5"
             />
           </WobbleCard>
-          <WobbleCard containerClassName="col-span-1 min-h-[300px]">
+          <WobbleCard containerClassName="col-span-1 min-h-[300px] bg-green-700">
             <h2 className="max-w-80 text-left text-base font-semibold tracking-[-0.015em] text-balance text-white md:text-xl lg:text-3xl">
               No need to use Ask Seniors Anymore.
             </h2>
-            <p className="mt-4 max-w-104 text-left text-base/6 text-neutral-200">
+            <p className="mt-4 text-left text-base/6 text-neutral-200">
               Just Sign Up with College Email and Experience.
             </p>
           </WobbleCard>
-          <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-blue-900 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
+          <WobbleCard containerClassName="col-span-1 lg:col-span-3 bg-cyan-600 min-h-[500px] lg:min-h-[600px] xl:min-h-[300px]">
             <div className="max-w-sm">
               <h2 className="max-w-sm text-left text-base font-semibold tracking-[-0.015em] text-balance text-white md:max-w-lg md:text-xl lg:text-3xl">
                 Signup for free and Join Astra AI Community today!
